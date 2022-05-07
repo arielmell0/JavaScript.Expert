dd7c299c796da2bb2328ffa7aaec5be3274a5d3a
arielmell0 <arielmello@rede.ulbra.br>
2022-05-07 04:18:11 -0300

    feat: testes na mao
    entendendo o valor dos nossos testes

--- src/file.js
+++ src/file.js
@@ -2,14 +2,17 @@ const { readFile } = require('fs/promises')
 const { join } = require('path')
 const { error } = require('./constants')
 
-const DEFAULT_OPTIONS = {
+const DEFAULT_OPTION = {
     maxLines: 3,
-    fields: [ "id", "name", "profession", "age"]
+    fields: [ "id", "name", "profession", "age" ]
 }
 
 class File {
     static async csvToJson(filePath) {
         const content = await File.getFileContent(filePath)
+        const validation = File.isValid(content)
+        if(!validation.valid) throw new Error(validation.error)
+
         return content
     }
 
@@ -20,18 +23,32 @@ class File {
 
     static isValid(csvString, options = DEFAULT_OPTION) {
         const [ header, ...fileWithoutHeader ] = csvString.split('\n')
-        const isHeaderValid = header === options.fields.join(',')
+        const isHeaderValid = header === options.fields.join(', ')
         if(!isHeaderValid) {
             return {
                 error: error.FILE_FIELDS_ERROR_MESSAGE,
                 valid: false
             }
         }
+
+        const isContentLengthAccepted = (
+            fileWithoutHeader.length > 0 &&
+            fileWithoutHeader.length <= options.maxLines
+        )
+        if(!isContentLengthAccepted) {
+            return {
+                error: error.FILE_LENGTH_ERROR_MESSAGE,
+                valid: false
+            }
+        }
+
+        return { valid: true }
     }
 }
 
 (async () => {
-    // const result = await File.csvToJson('../mocks/threeItems-valid.csv')
-    const result = await File.csvToJson('../mocks/fourItems-invalid.csv')
+    const result = await File.csvToJson('./../mocks/threeItems-valid.csv')
+    // const result = await File.csvToJson('./../mocks/fourItems-invalid.csv')
+    // const result = await File.csvToJson('../mocks/invalid-header.csv')
     console.log('result', result)
 })()
