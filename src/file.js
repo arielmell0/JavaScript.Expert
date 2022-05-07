b5860657331cd736c8feaa917386946b3e483f99
arielmell0 <arielmello@rede.ulbra.br>
2022-05-07 01:38:19 -0300

    feat: metodo estatico isValid
    separa o header do resto dos dados e faz a validacao

--- src/file.js
+++ src/file.js
@@ -1,5 +1,9 @@
 const { readFile } = require('fs/promises')
 const { join } = require('path')
+const DEFAULT_OPTIONS = {
+    maxLines: 3,
+    fields: [ "id", "name", "profession", "age"]
+}
 
 class File {
     static async csvToJson(filePath) {
@@ -11,9 +15,21 @@ class File {
         const filename = join(__dirname, filePath)
         return (await readFile(filename)).toString("utf8")
     }
+
+    static isValid(csvString, options = DEFAULT_OPTION) {
+        const [ header, ...fileWithoutHeader ] = csvString.split('\n')
+        const isHeaderValid = header === options.fields.join(',')
+        if(!isHeaderValid) {
+            return (
+                error: error.FILE_FIELDS_ERROR_MESSAGE,
+                valid: false
+            )
+        }
+    }
 }
 
 (async () => {
-    const result = await File.csvToJson('../mocks/threeItems-valid.csv')
+    // const result = await File.csvToJson('../mocks/threeItems-valid.csv')
+    const result = await File.csvToJson('../mocks/fourItems-invalid.csv')
     console.log('result', result)
 })()
