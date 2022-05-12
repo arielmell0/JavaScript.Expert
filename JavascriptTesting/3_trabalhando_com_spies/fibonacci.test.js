c573a2cad017a26c75c34823ccd17c1a87b0ef23
arielmell0 <arielmello@rede.ulbra.br>
2022-05-12 01:18:30 -0300

    feat: utilizando spies em algoritmos complexos
    inicio do projeto e criacao  do inicio da sequencia fibonacci

---
+++ JavascriptTesting/3_trabalhando_com_spies/fibonacci.test.js
@@ -0,0 +1,11 @@
+const Fibonacci = require('./fibonacci')
+const sinon = require('sinon')
+
+// Fibonacci: o próximo valor corresponde à soma dos dois anteriores
+// dado 3
+// 0, 1, 1
+// dado 5
+// 0, 1, 1, 2, 3
+(async () => {
+
+})()
