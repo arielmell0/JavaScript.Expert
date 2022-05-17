3011b62164bbc87d15d321e19f14c2c0332f11eb
arielmell0 <arielmello@rede.ulbra.br>
2022-05-16 23:50:36 -0300

    feat: contact and default page
    pagina contact:get e default page que retorna hello world caso alguma rota 404 ou a inicial seja acessada

---
+++ JavascriptTesting/4_coverage-e2e/api.js
@@ -0,0 +1,30 @@
+const http = require('http')
+
+const routes = {
+    '/contact:get': (request, response) => {
+        response.write('contact us page')
+        return response.end()
+    },
+
+    default: (request, response) => {
+        response.write('Hello World!')
+        return response.end()
+    }
+}
+
+const handler = function (request, response) {
+    const { url, method } = request
+    const routeKey = `${url}:${method.toLowerCase()}`
+    const chosen = routes[routeKey] || routes.default
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    })
+    return chosen(request, response)
+}
+
+const app = http.createServer(handler)
+    .listen(3000, () => {
+        console.log('app running at', 3000)
+    })
+
+module.exports = app
