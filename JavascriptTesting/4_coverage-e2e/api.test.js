658b8e0143560c1d00ee87e1fc56efb9f4d35e8c
arielmell0 <arielmello@rede.ulbra.br>
2022-05-24 00:39:08 -0300

    feat: teste 1 passando e teste 2 falhando
    o primeiro teste passa mas o segundo falha, pois o mocha nao consegue subir outro servidor para o segundo teste

--- JavascriptTesting/4_coverage-e2e/api.test.js
+++ JavascriptTesting/4_coverage-e2e/api.test.js
@@ -1,14 +1,25 @@
 const { describe, it } = require('mocha')
 const request = require('supertest')
 const app = require('./api')
+const assert = require('assert')
 
 describe('API Suite test', () => {
     describe('/contact', () => {
         it('should request the contact page and return HTTP Status 200', async () => {
+            const response = await request(app)
+                .get('/contact')
+                .expect(200)
+            assert.deepStrictEqual(response.text, 'contact us page')
+        })
+    })
+
+    describe('/hello', () => {
+        it('should request an inexistent route /hi and redirect to /hello', async() => {
             const response = await request('/app')
                 .get('/contact')
                 .expect(200)
-            console.log('response', response)
+
+        assert.deepStrictEqual(response.text, 'Hello world!')
         })
     })
 })
